412606cecd9372a8a4bc101bac49652b7fca6a08
Reind Dooyeweerd <info@vout.nl>
2026-02-16 07:42:34 +0100

    Initial Commit

---
+++ AI resource tracker/AI_resource_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_resource_trackerApp.swift
+//  AI resource tracker
+//
+//  Created by Reind Dooyeweerd on 16/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AI_resource_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
